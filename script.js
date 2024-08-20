1881c314ca654531a224c916dfd9b32ccf0c4709
fatimapervaiz34 <fatimahpervaiz95@gmail.com>
2024-08-20 21:42:22 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function myfunction(){
+
+   let name=document.getElementById("name").value;
+   let email=document.getElementById("email").value;
+   let password =document.getElementById("password").value;
+
+localStorage.setItem("n1","fatima");
+localStorage.setItem("eml","email");
+localStorage.setItem("pas","password");
+   
+}
+
+                                                                                                                                    
